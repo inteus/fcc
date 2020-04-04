2d611ef9211510ea77ebffaf862dd729ff3bd35c
Vlad <adhoc.nnov@gmail.com>
2020-04-04 16:02:37 +0300

    deploy to gh pages

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const store = createStore(
   reducers,
   compose(
       applyMiddleware(thunkMiddleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
